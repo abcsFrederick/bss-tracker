ad108eef754da4f6e11ef32de610cf342083b366
welfordj2 <josh.welford@nih.gov>
2024-03-15 18:47:53 -0400

    Adds export all button

--- app/Filament/Resources/SpecimenResource/Pages/ListSpecimens.php
+++ app/Filament/Resources/SpecimenResource/Pages/ListSpecimens.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\SpecimenResource\Pages;
 
 use App\Filament\Resources\SpecimenResource;
+use App\Models\Specimen;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Collection;
 
 class ListSpecimens extends ListRecords
 {
@@ -15,6 +17,27 @@ class ListSpecimens extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('exportAllAsJson')
+                ->label(__('Export All'))
+                ->action(function () {
+                    $records = Specimen::all();
+
+                    $archive = new \ZipArchive;
+
+                    $archive->open('specimens.zip', \ZipArchive::CREATE | \ZipArchive::OVERWRITE);
+
+                    foreach ($records as $record) {
+                        $name = \Str::slug($record->uid, '_') . '.json';
+                        $return = $record->load('sample.bioSample.project.investigator')->toArray();
+                        $content = json_encode($return, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_THROW_ON_ERROR);
+                        $archive->addFromString($name, $content);
+                    }
+
+                    $archive->close();
+
+                    return response()->download('specimens.zip');
+                })->hidden(fn() => Specimen::count() === 0),
+
             Actions\CreateAction::make(),
         ];
     }
